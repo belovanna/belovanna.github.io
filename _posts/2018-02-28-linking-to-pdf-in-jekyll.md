3994f0e2f28769677bfb4da45c49185c7f5b00c4
Anna Belova <anabelova@gmail.com>
2018-02-28 14:06:40 +0300

    post 28.02.18

---
+++ _posts/2018-02-28-linking-to-pdf-in-jekyll.md
@@ -0,0 +1,29 @@
+---
+layout: single
+title:  "Linking to a Pdf in Jekyll"
+excerpt: "Linking to a Pdf in Jekyll."
+date:   2018-02-27 15:42:32 +0300
+tags:
+  - website
+  - Jekyll
+categories: website update
+header:
+  image: /assets/images/banner.jpg
+  teaser: /assets/images/jekyll-logo.png
+---
+To make a pdf downloadable directly, place your pdf where you want it to be,
+say assets folder. Insert the code below anywhere on your page. Voilà,
+{% highlight markdown %}
+you can [download the pdf]({{ site.url }}/assets/mydoc.pdf) here.
+{% endhighlight %}
+
+An alternative is to have your pdf displayed on a Github webpage along with
+other content (check my CV page).
+
+* Make the static website/repository in Github: username.github.io
+* If you already have a website, create a new static page
+* Place your pdf where you want it to be (also root would do)
+* Embed your pdf file:
+{% highlight markdown %}
+<embed src="https://username.github.io/mydoc.pdf" type="application/pdf"/>
+{% endhighlight %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["website","update"],
         "tags": [],
         "url": "http://localhost:4000/website/update/welcome-to-jekyll/",
-        "teaser":"http://localhost:4000/assets/images/default-header.jpg"}]
+        "teaser":"http://localhost:4000/assets/images/default-header.jpg"},{
+        "title": "Linking to a Pdf in Jekyll",
+        "excerpt":"To make a pdf downloadable directly, place your pdf where you want it to be,say assets folder. Insert the code below anywhere on your page. Voilà,you can [download the pdf](http://localhost:4000/assets/mydoc.pdf) here.An alternative is to have your pdf displayed on a Github webpage along withother content (check my CV page). Make...","categories": ["website","update"],
+        "tags": ["website","Jekyll"],
+        "url": "http://localhost:4000/website/update/linking-to-pdf-in-jekyll/",
+        "teaser":"http://localhost:4000/assets/images/jekyll-logo.png"}]
