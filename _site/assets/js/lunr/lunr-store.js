dfa5c2abcb4c553c5fe8f8dc7b1211605adc2467
Anna Belova <anabelova@gmail.com>
2018-03-23 23:05:28 +0300

    content

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,21 +1,26 @@
 var store = [{
         "title": "Hello, World!",
         "excerpt":"Definition: “a “Hello, World!” program is a computer program that outputs or displays “Hello, World!” to a user. Being a very simple program in most programming languages, it is often used to illustrate the basic syntax of a programming language for a working program. It is often the very first...","categories": ["website","update"],
-        "tags": ["about","website"],
-        "url": "https://belovanna.github.io/website/update/hello-world/",
-        "teaser":"https://belovanna.github.io/assets/images/default-header.jpg"},{
+        "tags": ["data analysis"],
+        "url": "http://localhost:4000/website/update/hello-world/",
+        "teaser":"http://localhost:4000/assets/images/default-header.jpg"},{
         "title": "Linking to a Pdf in Jekyll",
-        "excerpt":"On this page Downloadable files Embedded files Downloadable filesTo make a pdf downloadable directly, place your pdf where you want it to be,say assets folder. Insert the code below anywhere on your page. Voilà,you can [download pdf](https://belovanna.github.io/assets/mydoc.pdf) here.Get the pdfEmbedded filesAn alternative is to have your pdf displayed on a...","categories": ["website","update"],
+        "excerpt":"On this page Downloadable files Embedded files Downloadable filesTo make a pdf downloadable directly, place your pdf where you want it to be,say assets folder. Insert the code below anywhere on your page. Voilà,you can [download pdf](http://localhost:4000/assets/mydoc.pdf) here.Get the pdfEmbedded filesAn alternative is to have your pdf displayed on a...","categories": ["website","update"],
         "tags": ["website","Jekyll"],
-        "url": "https://belovanna.github.io/website/update/linking-to-pdf-in-jekyll/",
-        "teaser":"https://belovanna.github.io/assets/images/jekyll-logo.png"},{
+        "url": "http://localhost:4000/website/update/linking-to-pdf-in-jekyll/",
+        "teaser":"http://localhost:4000/assets/images/jekyll-logo.png"},{
         "title": "Working with databases in R: dplyr",
-        "excerpt":"On this page Introduction Establishing a connection Intorduction to dplyr package Single table verbs Grouped operations Chaining Two-table verbs Mutating joins Types of mutating joins Filtering joins Set operations R to SQL translation Laziness IntroductionIn this post I offer an overview of some handy tools for working with databasesin R:...","categories": ["R","tutorial"],
-        "tags": ["R","dplyr","tutorial"],
-        "url": "https://belovanna.github.io/r/tutorial/working-with-databases-in-R/",
-        "teaser":"https://belovanna.github.io/assets/images/default-header.jpg"},{
+        "excerpt":"On this page Introduction Establishing a connection Intorduction to dplyr package Single table verbs Grouped operations Chaining Two-table verbs Mutating joins Types of mutating joins Filtering joins Set operations R to SQL translation Laziness IntroductionIn this post I offer an overview of some handy tools for working with databasesin R:...","categories": ["R"],
+        "tags": ["dplyr","tutorial"],
+        "url": "http://localhost:4000/r/working-with-databases-in-R/",
+        "teaser":"http://localhost:4000/assets/images/default-header.jpg"},{
         "title": "Web scraping",
         "excerpt":"What is web scrapingWebsites do not always provide their data in nice formats like JSON or csv.With economic data, it often happens that you need a dataset which only presented as anHTML table on a webpage and isn’t available for downloading. In such cases, the remedy cancome from web scraping....","categories": ["python"],
-        "tags": ["web scraping","parsing","python","data analysis"],
-        "url": "https://belovanna.github.io/python/scraping-from-html/",
-        "teaser":"https://belovanna.github.io/assets/images/scraping.jpg"}]
+        "tags": ["data analysis","tutorial"],
+        "url": "http://localhost:4000/python/scraping-from-html/",
+        "teaser":"http://localhost:4000/assets/images/scraping.jpg"},{
+        "title": "Steam video game analysis",
+        "excerpt":"Steam Video Games AnalysisCode Show All CodeHide All CodeBelow are some quick results of an exploratory analysis of Steam Data Set available on Kaggle. The dataset contains a single table with four columns: user_id, game_title, behaviour_name, values. Data description:“Steam is the world’s most popular PC Gaming hub. With a massive...","categories": ["R"],
+        "tags": ["data analysis","tutorial"],
+        "url": "http://localhost:4000/r/steam-video-game-analysis/",
+        "teaser":"http://localhost:4000/assets/images/default-header.jpg"}]
